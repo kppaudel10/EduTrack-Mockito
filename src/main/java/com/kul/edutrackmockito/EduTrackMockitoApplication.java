1789fb0eac925187e0885b53d4a99bf49aa49adc
Kul Paudel <kppaudel56@gmail.com>
2025-04-06 10:57:36 +0545

    feat(inital): initial project setup

---
+++ src/main/java/com/kul/edutrackmockito/EduTrackMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.kul.edutrackmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTrackMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduTrackMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kul/edutrackmockito/EduTrackMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kul.edutrackmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduTrackMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
